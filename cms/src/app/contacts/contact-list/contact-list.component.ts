8a932a44aec78afcb7c410f38317a54c20f074f4
CAMILA ROMERO <camilaar@byui.edu>
2022-09-24 13:18:59 -0400

    contactdetail

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -7,7 +7,10 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-  contacts: Contact[] = [];
+  contacts: Contact[] = [
+    new Contact('1', 'Bro.Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', 'null'),
+    new Contact('2', 'Bro.Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', 'null')
+  ];
 
 
   constructor() { }
--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,5 +1,19 @@
 export class Contact {
+    public id: string;
+    public name: string;
+    public email: string;
+    public phone: string;
+    public imgUrl: string;
+    public group: string;
     
-    constructor(public id: string, public name: string, public email: string, public phone: string, public imageUrl: string, public group: Contact[]) { }
+    constructor(id: string, name: string, email: string, phone: string, imgUrl: string, group: string) {
+        this.id =id;
+        this.name=name;
+        this.email=email;
+        this.phone=phone;
+        this.imgUrl=imgUrl;
+        this.group=group;
+        
+     }
 
 }
