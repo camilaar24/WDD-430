cad17cb657034084a4644c918200d15f5aba5842
CAMILA ROMERO <camilaar@byui.edu>
2022-09-29 12:20:37 -0400

    views

--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -8,9 +8,8 @@ import { Message } from '../message.model';
 })
 export class MessageListComponent implements OnInit {
   messages: Message[] = [
-    new Message('1', 'Test 1', 'This is the first test.', 'Aaron Tryle'),
-    new Message('2', 'Test 2', 'This is the second test.', 'Bobby Tryle'),
-    new Message('3', 'Test 3', 'This is the third test.', 'Charlie Tryle'),
+    new Message('1', 'Test 1', 'Message Test', 'Senders Name'),
+   
   ];
 
   constructor() {}
