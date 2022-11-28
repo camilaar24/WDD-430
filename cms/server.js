f85fbef9a07e0382632965bf895b1ae68092c7f1
CAMILA ROMERO <camilaar@byui.edu>
2022-11-27 20:54:49 -0500

    server and modules

--- cms/server.js
+++ cms/server.js
@@ -5,14 +5,26 @@ var http = require('http');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 // import the routing file to handle the default (index) route
 var index = require('./server/routes/app');
+// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
 const messageRoutes = require('./server/routes/messages');
 const contactRoutes = require('./server/routes/contacts');
-const documentsRoutes = require('./server/routes/documents');
+const documentsRoutes = require('./server/routes/documents'); 
 
-// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ... 
+// establish a connection to the mongo database
+
+mongoose.connect('mongodb+srv://camilaar:camilaar24.@cluster0.a63dcom.mongodb.net/test', 
+{ useNewUrlParser: true }, (err, res) => {
+  if (err) {
+    console.log('Could not connect to Database');
+  } else {
+    console.log('Connected to database...')
+  }
+});
+
 
 var app = express(); // create an instance of express
 
